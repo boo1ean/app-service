7055614d3483cd662619d23f2081a0e4dc2f5ac0
boo1ean <boo1ean0807@gmail.com>
2014-11-30 00:28:25 +0200

    Reimplement service wrapper

--- index.js
+++ index.js
@@ -1,26 +1,61 @@
 var _ = require('lodash');
+var Promise = require('bluebird');
 
-function compose (methods, validation) {
-	if (!_.isObject(methods)) {
-		throw new Error('Service must be an object');
+var defaults = {
+	methods: null,
+	validation: null,
+	before: null,
+	after: null
+};
+
+function noop () {}
+
+function configure (opts) {
+	if (!_.isObject(opts)) {
+		throw new Error('Service configuration should be an object');
 	}
 
-	if (!_.isObject(validation)) {
-		throw new Error('Validation must be an object');
+	defaults = _.extend(defaults, opts);
+}
+
+function compose (opts) {
+	if (!_.isObject(opts)) {
+		throw new Error('Service configuration should be an object');
 	}
 
-	return _.mapValues(methods, function forEachServiceMethod (method, methodName) {
-		if (_.isFunction(validation[methodName])) {
-			var validator = validation[methodName];
-			return function validatedServiceMethod () {
-				var args = arguments;
-				return validator.apply(this, args).then(function afterSuccessfulValidation () {
-					return method.apply(this, args);
+	if (!_.isObject(opts.methods)) {
+		throw new Error('Service methods should be an object');
+	}
+
+	_.defaults(opts, defaults);
+
+	var before = _.isFunction(opts.before) ? opts.before : noop;
+	var after = _.isFunction(opts.after) ? opts.after : noop;
+	var validation = _.isObject(opts.validation) ? opts.validation : {};
+
+	return _.mapValues(opts.methods, function forEachServiceMethod (method, methodName) {
+		var validator = validation[methodName] || noop;
+
+		return function serviceMethod () {
+			var returnValue;
+			var self = this;
+			var args = arguments;
+
+			return Promise.method(before).apply(self, args)
+				.then(function () {
+					return validator.apply(self, args);
+				})
+				.then(function () {
+					return method.apply(self, args);
+				})
+				.then(function (result) {
+					returnValue = result;
+					return after.apply(self, args);
+				})
+				.then(function () {
+					return returnValue;
 				});
-			}
-		}
-
-		return method;
+		};
 	});
 }
 
